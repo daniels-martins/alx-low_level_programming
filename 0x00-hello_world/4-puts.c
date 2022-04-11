88e393fb2c7487cea96dd6fd4e188abba1903a33
daniels-martins <danielsmrtns@gmail.com>
2022-04-11 15:23:10 -0700

    helper

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
  *main - function begins 
- *return: function ends
+ *
+ *Return: function ends
  */
-void  main(void)
+int main(void):
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
