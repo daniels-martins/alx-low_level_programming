69bbdc0cdc23893277afdd291f2658cc88fe5b29
daniels-martins <danielsmrtns@gmail.com>
2022-04-13 14:49:57 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,20 +7,21 @@
  */
 int print_sign(int n)
 {
-	char test[] = "0";
+	int test;
+
 	if (n > 0)
 	{
-		test = "1";
+		test = 1;
 		putchar('+');
 	}
 	else if (n == 0)
 	{
-		test = "0";
+		test = 0;
 		putchar('0');
 	}
 	else
 	{
-		test = "-1";
+		test = -1;
 		putchar('-');
 	}
 	return (test);
