a8642dca4cf6a7c23f200224bf014c7ec833bb95
daniels-martins <danielsmrtns@gmail.com>
2022-04-13 06:09:26 -0700

    edited task1 for betty style

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,10 +4,11 @@
  * print_alphabet - entry point
  * Return: always 0
  */
-void print_alphabet()
+void print_alphabet(void)
 {
 	char c = 'a';
-	while(c <= 'z')
+
+	while (c <= 'z')
 	{
 		putchar(c);
 		c++;
