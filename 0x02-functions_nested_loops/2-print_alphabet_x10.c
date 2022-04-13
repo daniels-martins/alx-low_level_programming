f8d967f58e05c292d5c545172aed441654d0b596
daniels-martins <danielsmrtns@gmail.com>
2022-04-13 07:23:30 -0700

    edited

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_alphabet_10x - this program pints the letters of the alphabets 10x
+ * print_alphabet_x10 - this program pints the letters of the alphabets 10x
  * Return: always 0
  */
 void print_alphabet_x10(void)
 {
 	char c = 'a';
 	int limit  = 10;
-	int current = 1;
-	
+	int current = 1;	
 	while (current <= limit)
 	{
 		c = 'a';
