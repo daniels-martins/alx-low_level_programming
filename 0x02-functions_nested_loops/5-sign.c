266398768adae11e88dbe7ca6547619108e11930
daniels-martins <danielsmrtns@gmail.com>
2022-04-13 14:04:50 -0700

    task5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_sign - check numbers
+ * @n : number to check
+ * Return: 0 or 1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		test = 1;
+		putchar('+);
+	}
+	else if (n == 0)
+	{
+		test = 0;
+		putchar('0');
+	}
+	else
+	{
+		test = -1;
+		putchar('-');
+	}
+	return (test);
+}
